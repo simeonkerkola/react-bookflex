35c8e49425f2b13cac2e8bf67cbc53c0860d4218
sssmi <simeon.kerkola@gmail.com>
2018-04-27 22:03:55 +0300

    Make sidebar sticky

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,43 +1,51 @@
 import React from 'react';
+import Sticky from 'react-stickynode';
 
 const Sidebar = () => (
   <nav className="sidebar">
-    <ul className="side-nav">
-      <li className="side-nav__item side-nav__item--active">
-        <a href="#0" className="side-nav__link">
-          <svg className="side-nav__icon">
-            <use xlinkHref="/img/sprite.svg#icon-home" />
-          </svg>
-          <span>Hotel</span>
-        </a>
-      </li>
-      <li className="side-nav__item">
-        <a href="#0" className="side-nav__link">
-          <svg className="side-nav__icon">
-            <use xlinkHref="/img/sprite.svg#icon-aircraft-take-off" />
-          </svg>
-          <span>Flight</span>
-        </a>
-      </li>
-      <li className="side-nav__item">
-        <a href="#0" className="side-nav__link">
-          <svg className="side-nav__icon">
-            <use xlinkHref="/img/sprite.svg#icon-key" />
-          </svg>
-          <span>Car rental</span>
-        </a>
-      </li>
-      <li className="side-nav__item">
-        <a href="#0" className="side-nav__link">
-          <svg className="side-nav__icon">
-            <use xlinkHref="/img/sprite.svg#icon-map" />
-          </svg>
-          <span>Tours</span>
-        </a>
-      </li>
-    </ul>
+    <Sticky
+      // Disable for mobile
+      enabled={!(window.innerWidth <= 900)}
+    >
+      <ul className="side-nav">
+        <li className="side-nav__item side-nav__item--active">
+          <a href="#0" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref="/img/sprite.svg#icon-home" />
+            </svg>
+            <span>Hotel</span>
+          </a>
+        </li>
+        <li className="side-nav__item">
+          <a href="#0" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref="/img/sprite.svg#icon-aircraft-take-off" />
+            </svg>
+            <span>Flight</span>
+          </a>
+        </li>
+        <li className="side-nav__item">
+          <a href="#0" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref="/img/sprite.svg#icon-key" />
+            </svg>
+            <span>Car rental</span>
+          </a>
+        </li>
+        <li className="side-nav__item">
+          <a href="#0" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref="/img/sprite.svg#icon-map" />
+            </svg>
+            <span>Tours</span>
+          </a>
+        </li>
+      </ul>
+    </Sticky>
 
-    <div className="legal">&copy; {new Date().getFullYear()} Bookflex</div>
+    <div className="legal" id="legal">
+        &copy; {new Date().getFullYear()} Bookflex
+    </div>
   </nav>
 );
 
